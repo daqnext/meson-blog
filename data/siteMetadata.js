06c1940f7c08ebfea87a4032b73c3df98e71e6b9
zionfuo <bitruss@protonmail.com>
2022-07-04 20:23:41 +0800

    Update siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -22,7 +22,7 @@ const siteMetadata = {
   analytics: {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
-    googleAnalyticsId: '', // e.g. UA-000000-2 or G-XXXXXXX
+    googleAnalyticsId: 'G-H4QX5MJMK8', // e.g. UA-000000-2 or G-XXXXXXX
   },
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
