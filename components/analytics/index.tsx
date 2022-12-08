d96fce5e14655fd8e2259dcbf089b82cc5326a6a
zionfuo <bitruss@protonmail.com>
2022-12-08 11:52:16 +0800

    Update index.tsx

--- components/analytics/index.tsx
+++ components/analytics/index.tsx
@@ -1,12 +1,17 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import GA from './GoogleAnalytics'
 import MA from './MatomoAnalytics'
+import Plausible from './Plausible'
+import SimpleAnalytics from './SimpleAnalytics'
+import Umami from './Umami'
 import siteMetadata from '@/data/siteMetadata'
 
 declare global {
   interface Window {
     gtag?: (...args: any[]) => void
     _paq?: (...args: any[]) => void
+    plausible?: (...args: any[]) => void
+    sa_event?: (...args: any[]) => void
   }
 }
 
@@ -15,6 +20,9 @@ const isProduction = process.env.NODE_ENV === 'production'
 const Analytics = () => {
   return (
     <>
+      {isProduction && siteMetadata.analytics.plausibleDataDomain && <Plausible />}
+      {isProduction && siteMetadata.analytics.simpleAnalytics && <SimpleAnalytics />}
+      {isProduction && siteMetadata.analytics.umamiWebsiteId && <Umami />}
       {isProduction && siteMetadata.analytics.googleAnalyticsId && <GA />}
       {isProduction && siteMetadata.analytics.matomoAnalyticsId && <MA />}
     </>
