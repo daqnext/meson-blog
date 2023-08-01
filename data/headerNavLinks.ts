20abb83f2b447b4d5369c79adf52671f98c7cba2
Sherlock <shh_name@hotmail.com>
2023-08-01 15:40:55 +0800

    Fix Displays (#23)

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,6 +1,5 @@
 const headerNavLinks = [
   { href: 'https://meson.network/', title: 'Home' },
-  { href: '/blog', title: 'News' },
   { href: 'https://meson.network/ecosystem.html', title: 'Ecosystem' },
   { href: 'https://docs.meson.network/', title: 'Docs' },
   { href: 'https://meson.network/about.html', title: 'About' },
